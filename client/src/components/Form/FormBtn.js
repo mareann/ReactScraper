ff05ae4e122166a0901f557706452d401a9cc4f3
Maryann Jordan <majch@DESKTOP-KQJEU0U.(none)>
2018-04-28 18:24:11 -0500

    remove unused files

--- client/src/components/Form/FormBtn.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const FormBtn = (props) => (
-  <button {...props} onClick={props.onLoadClick} style={{ marginBottom: 10 }} className="btn btn-success">
-    {props.children}
-  </button>
-);
-
-export default FormBtn;
